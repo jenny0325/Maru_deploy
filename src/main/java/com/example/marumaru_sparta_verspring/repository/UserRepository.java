c71218b589f24dd3bcef51374973091568ee5323
Jenny <imj1996@naver.com>
2021-12-10 09:45:53 +0900

    백엔드 배포 연습

--- src/main/java/com/example/marumaru_sparta_verspring/repository/UserRepository.java
+++ src/main/java/com/example/marumaru_sparta_verspring/repository/UserRepository.java
@@ -9,5 +9,4 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByKakaoId(Long kakaoId);
-    Optional<User> findByNickname(String nickname);
 }
